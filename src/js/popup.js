c991ab30dacbf251cf4050eb723b90c6f24c37fb
Hisashi Iwamoto <hisasi622@gmail.com>
2023-07-29 10:45:27 +0900

    add image and btn action

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,31 +1,35 @@
-chrome.identity.getAuthToken({ interactive: true }, function (token) {
-  var event = {
-    summary: "Sample Event",
-    start: {
-      dateTime: "2023-08-15T09:00:00-07:00",
-      timeZone: "America/Los_Angeles",
-    },
-    end: {
-      dateTime: "2023-08-15T17:00:00-07:00",
-      timeZone: "America/Los_Angeles",
-    },
-  };
+const btn = document.getElementById("btn");
 
-  var xhr = new XMLHttpRequest();
-  xhr.open(
-    "POST",
-    "https://www.googleapis.com/calendar/v3/calendars/primary/events?access_token=" +
-      token
-  );
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onload = function () {
-    if (xhr.status == 200) {
-      // Success
-      console.log(xhr.responseText);
-    } else {
-      // Failure
-      console.log("Error " + xhr.statusText);
-    }
-  };
-  xhr.send(JSON.stringify(event));
+btn.addEventListener("click", () => {
+  chrome.identity.getAuthToken({ interactive: true }, function (token) {
+    var event = {
+      summary: "Sample Event",
+      start: {
+        dateTime: "2023-08-15T09:00:00-07:00",
+        timeZone: "America/Los_Angeles",
+      },
+      end: {
+        dateTime: "2023-08-15T17:00:00-07:00",
+        timeZone: "America/Los_Angeles",
+      },
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open(
+      "POST",
+      "https://www.googleapis.com/calendar/v3/calendars/primary/events?access_token=" +
+        token
+    );
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = function () {
+      if (xhr.status == 200) {
+        // Success
+        console.log(xhr.responseText);
+      } else {
+        // Failure
+        console.log("Error " + xhr.statusText);
+      }
+    };
+    xhr.send(JSON.stringify(event));
+  });
 });
